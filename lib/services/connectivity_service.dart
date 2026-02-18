722771e2cb80cbdeedb869f2dc93459ba2783581
michalpolak71 <michalpolak71@gmail.com>
2026-02-18 11:26:33 +0100

    v1.0 APK build ready

--- lib/services/connectivity_service.dart
+++ lib/services/connectivity_service.dart
@@ -1,11 +1,8 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 
 class ConnectivityService {
-  /// Sprawdź czy jest dostęp do internetu (dane/WiFi)
   static Future<bool> hasInternet() async {
     final connectivityResult = await Connectivity().checkConnectivity();
-    return connectivityResult.contains(ConnectivityResult.mobile) ||
-        connectivityResult.contains(ConnectivityResult.wifi) ||
-        connectivityResult.contains(ConnectivityResult.ethernet);
+    return connectivityResult != ConnectivityResult.none;
   }
 }
